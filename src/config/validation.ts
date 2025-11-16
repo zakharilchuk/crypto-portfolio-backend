4194ce04f13062ca2841a9a3003b702db0afc31e
Zakhar Ilchuk <zakharilchuk1@gmail.com>
2025-11-16 21:19:59 +0200

    add default values for db env variables

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 export const validationSchema = Joi.object({
   APP_PORT: Joi.number().default(8080),
-  POSTGRES_USER: Joi.string().required(),
-  POSTGRES_PASSWORD: Joi.string().required(),
-  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USER: Joi.string().default('postgres'),
+  POSTGRES_PASSWORD: Joi.string().default('password'),
+  POSTGRES_DB: Joi.string().default('crypto_portfolio'),
   POSTGRES_PORT: Joi.number().default(5432),
   POSTGRES_HOST: Joi.string().default('localhost'),
 });
